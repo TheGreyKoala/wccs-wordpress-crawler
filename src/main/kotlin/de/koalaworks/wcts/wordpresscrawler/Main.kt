c2fc28b003246fda50820ad1b8d827d03ad2513b
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-06-29 21:15:05 +0200

    Annotate additional site

--- src/main/kotlin/de/koalaworks/wcts/wordpresscrawler/Main.kt
+++ src/main/kotlin/de/koalaworks/wcts/wordpresscrawler/Main.kt
@@ -3,12 +3,21 @@ package de.koalaworks.wcts.wordpresscrawler
 import com.mashape.unirest.http.Unirest
 
 fun main(args: Array<String>) {
-    val result = Unirest
+    val result1 = Unirest
             .post("http://localhost:44284/analyse")
             .header("Content-Type", "application/json")
             .body("""{"url": "http://www.fernuni-hagen.de/KSW/portale/babw/service/"}""")
-            .asJson()
+            .asJsonAsync()
 
-    val message = if (result.status == 201) "Success" else "Error"
+    val result2 = Unirest
+            .post("http://localhost:44284/analyse")
+            .header("Content-Type", "application/json")
+            .body("""{"url": "http://www.fernuni-hagen.de/KSW/portale/babw/service/aktuelles/"}""")
+            .asJsonAsync();
+
+    val response1 = result1.get()
+    val response2 = result2.get()
+
+    val message = if (response1.status == 201 && response2.status === 201) "Success" else "Error"
     println(message)
 }
