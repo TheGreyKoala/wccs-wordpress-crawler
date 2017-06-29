057dcee8265f7899bb8a2bbe7be9a115662b030f
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-06-29 22:03:15 +0200

    Allow to annotate two different pages

--- src/main/kotlin/de/koalaworks/wcts/wordpresscrawler/Main.kt
+++ src/main/kotlin/de/koalaworks/wcts/wordpresscrawler/Main.kt
@@ -1,23 +1,28 @@
 package de.koalaworks.wcts.wordpresscrawler
 
+import com.mashape.unirest.http.HttpResponse
+import com.mashape.unirest.http.JsonNode
 import com.mashape.unirest.http.Unirest
 
 fun main(args: Array<String>) {
-    val result1 = Unirest
-            .post("http://localhost:44284/analyse")
-            .header("Content-Type", "application/json")
-            .body("""{"url": "http://www.fernuni-hagen.de/KSW/portale/babw/service/"}""")
-            .asJsonAsync()
+    val result: HttpResponse<JsonNode>
+    if ("1" == args[0]) {
+        result = Unirest
+                .post("http://localhost:44284/analyse")
+                .header("Content-Type", "application/json")
+                .body("""{"url": "http://www.fernuni-hagen.de/KSW/portale/babw/service/"}""")
+                .asJson()
 
-    val result2 = Unirest
-            .post("http://localhost:44284/analyse")
-            .header("Content-Type", "application/json")
-            .body("""{"url": "http://www.fernuni-hagen.de/KSW/portale/babw/service/aktuelles/"}""")
-            .asJsonAsync();
+        val message = if (result.status == 201) "Success" else "Error"
+        println("Service: " + message)
+    } else if ("2" == args[0]) {
+        result = Unirest
+                .post("http://localhost:44284/analyse")
+                .header("Content-Type", "application/json")
+                .body("""{"url": "http://www.fernuni-hagen.de/KSW/portale/babw/service/aktuelles/"}""")
+                .asJson()
 
-    val response1 = result1.get()
-    val response2 = result2.get()
-
-    val message = if (response1.status == 201 && response2.status === 201) "Success" else "Error"
-    println(message)
+        val message = if (result.status == 201) "Success" else "Error"
+        println("Aktuelles: " + message)
+    }
 }
